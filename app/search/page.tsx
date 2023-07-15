99516977bf8e32635a9fc0c257f6af83740cd02d
Sahasrast <sahasrast@gmail.com>
2023-07-15 00:21:46 -0400

    i need to figure out some things tmr

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,13 +1,18 @@
 "use client"
 import { useState } from "react"
-
+import Link from "next/link"
 
 export default function Search (){
     const [searchText, setsearchText] = useState("Search for miRNAs")
+    onkeydown
     return(<>
     <div  className= "flex justify-center items-center">
-        <input value= {searchText} onChange={(e)=>setsearchText(e.target.value)} className="bg-gray-50 shadow rounded pr-[350px] pl-[5px] py-[4px] text-left text-black"/>
-        
+       <form action="/searchresults" text-black> 
+            <label htmlFor = "searchbar" >Search Bar:   </label>
+            <input type="text" id="searchbar" name="searchbar"/>
+            <input type="submit" value="Submit"/>
+       </form>
     </div>
     </>)
 }
+//
