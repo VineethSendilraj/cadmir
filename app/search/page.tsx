37ae1bb371497c7cd43b8cacaad06486cc8efb78
Sahasrast <sahasrast@gmail.com>
2023-07-15 14:26:14 -0400

    enter

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,18 +1,13 @@
 "use client"
-import { useState } from "react"
-import Link from "next/link"
 
 export default function Search (){
-    const [searchText, setsearchText] = useState("Search for miRNAs")
-    onkeydown
+
     return(<>
-    <div  className= "flex justify-center items-center">
        <form action="/searchresults" text-black> 
             <label htmlFor = "searchbar" >Search Bar:   </label>
             <input type="text" id="searchbar" name="searchbar"/>
             <input type="submit" value="Submit"/>
        </form>
-    </div>
     </>)
 }
 //
---
+++ app/searchresults/page.tsx
@@ -0,0 +1,19 @@
+
+export default function searchresults (){
+    return (<>
+        <table> 
+            <thead>
+                <td>miRNA</td>
+                <td>Symptoms</td>
+                <td>Some</td>
+                <td>something else</td>
+                <td>idk</td>
+            </thead>
+            
+        </table>
+
+
+
+
+   </> )
+}
